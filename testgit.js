e5b2820e6cc9bcf6b292526de6353748ee3d07bc
Mari <zhelezniakmariia@gmail.com>
2021-03-05 19:49:09 +0200

    added srcond button

--- testgit.js
+++ testgit.js
@@ -1,18 +1,28 @@
 let a, b, c;
 let button = document.getElementById("color");
+let font = document.getElementById("font");
 
 button.addEventListener("click", changeColor);
+font.addEventListener("click", changeFontColor);
 
 function changeColor() {
 
-    a = getRandomInt(0, 256);
-    b = getRandomInt(0, 256);
-    c = getRandomInt(0, 256);
-    button.style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
+    a = getRandomInt(100, 256);
+    b = getRandomInt(100, 256);
+    c = getRandomInt(100, 256);
+    font.style.backgroundColor = `rgb(${a}, ${b}, ${c})`;
 }
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}
+
+function changeFontColor() {
+
+    a = getRandomInt(100, 256);
+    b = getRandomInt(100, 256);
+    c = getRandomInt(100, 256);
+    button.style.color = `rgb(${a}, ${b}, ${c})`;
 }
